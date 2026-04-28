0ffa9d7cfc64fbbb50e21831117e483f0e599844
Ubuntu <ubuntu@ip-172-31-16-15.ec2.internal>
2026-04-28 09:56:14 +0000

    Correcto funcionamiento de ambas funciones

--- backend/app/descuentos.py
+++ backend/app/descuentos.py
@@ -2,13 +2,12 @@ def calcular_precio_final(precio, descuento):
     
     precio_final = 0
     
-    precio = 5
-    
-    descuento = 0,20
     
     precio_final = precio + (precio * descuento)
     print(precio_final)
     
     return
 
-calcular_precio_final()
+calcular_precio_final(5, 0.20)
+
+calcular_precio_final(5, 0.50)
