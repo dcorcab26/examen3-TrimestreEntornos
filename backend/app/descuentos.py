073ffa9f9045ba007c377c21c1808ca89bbb8882
Ubuntu <ubuntu@ip-172-31-16-15.ec2.internal>
2026-04-28 09:52:27 +0000

    Primer descuento terminado

---
+++ backend/app/descuentos.py
@@ -0,0 +1,14 @@
+calcular_precio_final(precio, descuento):
+    
+    precio_final = 0
+    
+    precio = 5
+    
+    descuento = 0,20
+    
+    precio_final = precio + (precio * descuento)
+    print(precio_final)
+    
+    return
+
+calcular_precio_final()
